0cae2d48fdf0bdf9743f3c9832b3b2b5c14f9739
bennykoval <bethanykoval@gmail.com>
2019-06-05 12:56:51 -0400

    Open file, print content

--- word_frequency.py
+++ word_frequency.py
@@ -4,24 +4,41 @@ STOP_WORDS = [
     'will', 'with'
 ]
 
+#import re
 
-def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
+#def split_string(file):
+    #"""Read in `file` and split strings in that file."""
 
+#def print_word_freq(file):
+    #"""Read in `file` and print out the frequency of words in that file."""
+    #pass
 
-if __name__ == "__main__":
-    import argparse
-    from pathlib import Path
+#with open(file) as chosen_file:
+   # for line in f:
+    #string_split = line.split()
+    #print(repr(file.readline()))
+    #len(chosen_file.readline())
 
-    parser = argparse.ArgumentParser(
-        description='Get the word frequency in a text file.')
-    parser.add_argument('file', help='file to read')
-    args = parser.parse_args()
+with open(file) as chosen_file:
+    chosen_file = print(repr(file.readline()))
 
-    file = Path(args.file)
-    if file.is_file():
-        print_word_freq(file)
-    else:
-        print(f"{file} does not exist!")
-        exit(1)
+#with open('flights.txt', 'r') as f:
+    #for line in f:
+        #y = line.split()
+        #print(y)
+
+#if __name__ == "__main__":
+    #import argparse
+    #from pathlib import Path
+
+    #parser = argparse.ArgumentParser(
+        #description='Get the word frequency in a text file.')
+    #parser.add_argument('file', help='file to read')
+    #args = parser.parse_args()
+
+    #file = Path(args.file)
+    #if file.is_file():
+        #print_word_freq(file)
+    #else:
+        #print(f"{file} does not exist!")
+        #exit(1)
