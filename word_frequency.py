3aaaae81f5d6c8487b5cef219277f5435785bd41
bennykoval <bethanykoval@gmail.com>
2019-06-06 16:47:46 -0400

    Words sorted by frequency

--- word_frequency.py
+++ word_frequency.py
@@ -15,23 +15,33 @@ def clean_text(text):
 
 def split_text(text):
     """Split string into words"""
-    text = (str(text)).split(" ")
-    return text
+    ls_text = (str(text)).split(" ")
+    return ls_text
 
-def block_words(text):
+def block_words(word_list):
     """Delete overly common link words"""
     new_list = []
-    for i in text:
-        if i not in stop_words:
-            new_list.append(i)
+    for word in word_list:
+        if word not in stop_words:
+            new_list.append(word)
     return new_list
+
+def get_dict(word_list):
+    word_dict = {}
+    for word in word_list:
+        if word_dict.get(word) == None:
+            word_dict[word] = 1
+        else:
+            word_dict[word] += 1
+    return word_dict
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
     cleanest_text = clean_text(file)
-    splittest_text = split_text(cleanest_text)
-    blockiest_text = block_words(splittest_text)
-    return blockiest_text
+    split_ls = split_text(cleanest_text)
+    block_ls = block_words(split_ls)
+    word_freq = get_dict(block_ls)
+    return word_freq
 
 if __name__ == "__main__":
     import argparse
@@ -50,5 +60,5 @@ if __name__ == "__main__":
         exit(1)
 
 with open(file) as chosen_file:
-    blockiest_text = print_word_freq(chosen_file.read())
-    print(blockiest_text)
+    word_freq = print_word_freq(chosen_file.read())
+    print(word_freq)
