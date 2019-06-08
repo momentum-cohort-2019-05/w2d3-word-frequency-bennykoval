77163c53abf5f43746755f0b26e1c5e2136a3e04
bennykoval <bethanykoval@gmail.com>
2019-06-08 17:16:53 -0400

    Added some more stop words, including 'but' and 'was', also trying to delete those three pesky spaces

--- word_frequency.py
+++ word_frequency.py
@@ -1,7 +1,7 @@
 stop_words = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
-    'will', 'with', 'they', 'which', 'this', 'their'
+    'will', 'with', 'they', 'which', 'this', 'their', 'but', 'was', '   '
 ]
 
 import re
