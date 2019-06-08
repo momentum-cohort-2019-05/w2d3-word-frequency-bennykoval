fcdb81062029fe5b8c864ec824e659adf7519177
bennykoval <bethanykoval@gmail.com>
2019-06-08 17:15:47 -0400

    Added some more stop words like they, which, etc

--- word_frequency.py
+++ word_frequency.py
@@ -1,7 +1,7 @@
 stop_words = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
-    'will', 'with'
+    'will', 'with', 'they', 'which', 'this', 'their'
 ]
 
 import re
